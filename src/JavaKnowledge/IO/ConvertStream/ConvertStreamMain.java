7a4c18daa49c0d878d467437f1b6944d6cfbfef5
Wailiam-li <1374438718@qq.com>
2023-02-22 17:19:55 +0800

    2.122 下午学习

---
+++ src/JavaKnowledge/IO/ConvertStream/ConvertStreamMain.java
@@ -0,0 +1,14 @@
+package JavaKnowledge.IO.ConvertStream;
+
+/*
+    转换流： 是字符流和字节流之间的桥梁。
+            主要作用：在字节流中想使用字符流中的方法；
+ */
+public class ConvertStreamMain {
+    public static void main(String[] args) {
+
+        /*
+          1.FileReader可以 指定字符编码读取数据（JDK11.0以后），同理对FileWriter可以指定字符编码写出。
+         */
+    }
+}
