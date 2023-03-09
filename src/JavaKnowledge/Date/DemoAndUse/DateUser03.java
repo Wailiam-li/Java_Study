431941efddd3a42f24f9f42b18013976f8503a18
Wailiam-li <1374438718@qq.com>
2023-03-09 17:41:19 +0800

    2023.3.9

--- src/JavaKnowledge/Date/DemoAndUse/DateUser03.java
+++ src/JavaKnowledge/Date/DemoAndUse/DateUser03.java
@@ -15,32 +15,20 @@ public class DateUser03 {
 
     public static void main(String[] args) throws ParseException {
 
-//        String date1="2023-02-12";
-//
-//        String date2="2023-03-12";
-//
-//        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-mm-dd");
-//
-//        long l_date1=sdf.parse(date1).getTime();
-//
-//        long l_date2=sdf.parse(date2).getTime();
-//
-//        int day=(int)((l_date2-l_date1)/(1000*60*60*24));
-//
-//        System.out.println(day);
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
+        Date date=new Date();
+        calendar.setTime(date);
         long time1 = calendar.getTimeInMillis();
         //这里需要把那个人的生日移过来，即将他的出生年 改成 当前年
-        String sdate01="1999-10-12";
+        String sdate01="1999-03-12";
         StringBuilder stringBuilder=new StringBuilder();
         String date01 = stringBuilder.append(sdf.format(new Date()).substring(0, 4)).append(sdate01.substring(4, 10)).toString();
         Date d2 = sdf.parse(date01);
         calendar.setTime(d2);
         long time2 = calendar.getTimeInMillis();
         //需要加上判断，如果是time1 - time2>0表示生日在当今以后，如果小于0，则不计算，表示已过
-        if(time2 - time1 > 0) {
+        if(time2 - time1 >= 0) {
             long subDay = (time2 - time1) / (1000 * 60 * 60 * 24);
             System.out.println(subDay);
         }
