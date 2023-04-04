499f8792ec6470999e75565b6f599fd851d4a60e
Wailiam-li <1374438718@qq.com>
2023-04-04 14:08:23 +0800

    2023.4.4

--- src/JavaKnowledge/abc/test.java
+++ src/JavaKnowledge/abc/test.java
@@ -1,4 +1,16 @@
 package JavaKnowledge.abc;
 
+import java.math.BigDecimal;
+
 public class test {
+    public static void main(String[] args) {
+        /*
+         四舍五入保留两位用法
+         */
+        double one = 5.26519;
+        BigDecimal two = new BigDecimal(one);
+        double three = two.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
+        System.out.println(three);
+    }
+
 }
