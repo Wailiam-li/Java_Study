ace81955174ccb3c6051cdd8f5b01c98d1e1e650
Wailiam-li <1374438718@qq.com>
2023-05-30 09:25:54 +0800

    2023.5.30

--- src/JavaKnowledge/abc/Draft.java
+++ src/JavaKnowledge/abc/Draft.java
@@ -1,15 +1,55 @@
 package JavaKnowledge.abc;
 
+import JavaKnowledge.Oop.Super.Person;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 public class Draft {
     public static void main(String[] args) throws ParseException {
-        String date = "2023-04-24";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date parse = sdf.parse(date);
-        long time = parse.getTime();
-        System.out.println(time);
+//        String date = "";
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        Date parse = sdf.parse(date);
+////        long time = parse.getTime();
+//        System.out.println(parse);
+
+
+//    }
+
+//        StringBuilder builder=new StringBuilder();
+//        Map<String, String> m = new HashMap<>();
+//        System.out.println(m);
+//        System.out.println(m.get("code"));
+
+//        List<String> s=new ArrayList<>();
+//        System.out.println(s.size());
+
+        Set<Long> l=new HashSet<Long>() {
+        };
+//        personList.add(new Person("liwei",23));
+//        personList.add(new Person("liwei",23));
+//        personList.add(new Person("lihui",28));
+
+//        personList.sort(Comparator.comparing(Person::getAge));
+         l.add(1L);
+         l.add(1L);
+         l.add(1L);
+         l.add(1L);
+        System.out.println(l);
+
+        TreeMap tm=new TreeMap<>();
+        HashMap hm=new HashMap();
+        hm.put(1,"aaaa");
+        hm.put(9,"cccc");
+        hm.put(2,"cc");
+        hm.put(5,"dd");
+        hm.put(3,"dd");
+        tm.putAll(hm);
+
+        System.out.println(tm);
     }
+
+
+
 }
