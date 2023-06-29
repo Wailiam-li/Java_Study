adb3f081d7e336e61adcbfb9e701a2722c09a6c2
Wailiam-li <1374438718@qq.com>
2023-06-29 15:59:46 +0800

    2023.6.29  周四  下午

--- src/JavaKnowledge/abc/test.java
+++ src/JavaKnowledge/abc/test.java
@@ -2,6 +2,7 @@ package JavaKnowledge.abc;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 public class test {
@@ -43,26 +44,82 @@ public class test {
     }
 
     @Test
-    public void test(){
+    public void test() {
 
         List<Integer> list1 = Arrays.asList(1, 2, 3);
-        List<Integer> list2 = Arrays.asList(3,4,5);
+        List<Integer> list2 = Arrays.asList(3, 4, 5);
 
 //        Collection union = CollectionUtils.union(list1, list2);
         System.out.println("unionTest1 并集 :" + list1);
     }
 
 
+    @Test
+    public void test01() {
+        BigDecimal a = new BigDecimal("1.1225");
+        BigDecimal b = new BigDecimal("1.122");
+        BigDecimal c = new BigDecimal("0");
+        BigDecimal d = a.add(b).add(c).setScale(2, BigDecimal.ROUND_HALF_UP);
+        System.out.println(d);
+    }
 
+    @Test
+    public void test02() {
+      String a="XXXX支行";
+      if ('1'=='1' && a.charAt(0)==a.charAt(1)&&a.charAt(1)==a.charAt(2)){
+          System.out.println("true");
+      }
+    }
 
+    @Test
+    public void test03() {
+        String a="XXXX支行";
+        if ('1'=='1' && a.substring(a.length()-2).equals("支行")){
+            System.out.println("true");
+        }
+    }
 
+    @Test
+    public void test04() {
+        String str = "i love 791202";
+        System.out.println(str.substring(str.length() - 6)); // 791202
+    }
 
+    @Test
+    public void test05() {
+        String str = "111111111a";
+        if (str.matches("\\d{9,10}") == false) {
+            System.out.println("输入有误"); // 791202
+        }else{
+            System.out.println("无误");
+        }
+    }
 
+    @Test
+    public void test06() {
+        String str = "ssss分行";
+        if (!str.substring(str.length()-2).equals("支行")&&
+                !str.substring(str.length()-2).equals("分行")) {
+            System.out.println("输入有误");
+        }else{
+            System.out.println("无误");
+        }
+    }
 
-
-
-
-
-
-
+    @Test
+    public void test07() {
+        String str = "1111";
+        if ('a'=='a'&&(str.charAt(0)==str.charAt(1)&&str.charAt(2)==str.charAt(1)
+        &&str.charAt(2)==str.charAt(3))) {
+            System.out.println("数字不能全部一样");
+        }else{
+            System.out.println("无误");
+        }
+    }
 }
+
+
+
+
+
+
