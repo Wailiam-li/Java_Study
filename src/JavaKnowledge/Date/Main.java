499c541aa0e287c353b1df1c2306a45ae5db848f
Wailiam-li <1374438718@qq.com>
2023-08-07 09:39:08 +0800

    2023.08.07  周五  下午

--- src/JavaKnowledge/Date/Main.java
+++ src/JavaKnowledge/Date/Main.java
@@ -2,6 +2,7 @@ package JavaKnowledge.Date;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,50 +16,66 @@ public class Main {
          */
 
     @Test
-    public void test(){
+    public void test() {
 
-        String date="2023-06-09";
-        SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd ");
-        Date d=new Date();
+        String date = "2023-06-09";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd ");
+        Date d = new Date();
 
-        Date d2=new Date(2023,9,5);
+        Date d2 = new Date(2023, 9, 5);
 
 
-        System.out.println("Date 'd' is before Date 'd2' : "+d.before(d2));
+        System.out.println("Date 'd' is before Date 'd2' : " + d.before(d2));
 
     }
 
 
     @Test
-    public void test01(){
+    public void test01() throws ParseException {
 
-//        String date="2023-06-09";
-        SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date d=new Date();
-        d.setTime(1690353424*1000L);
-        String format = sdf.format(d);
-//        Date d2=new Date(2023,9,5);
+        String date = "2021-07-31";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date parse = sdf.parse(date);
 
-        System.out.println("时间是："+d);
-        System.out.println("时间是："+format);
+        Date d = new Date();
+        long oneYearAgo = parse.getTime();
+        long now = d.getTime();
+        System.out.println(oneYearAgo);
+        System.out.println(now);
+
+        long l = now - oneYearAgo;
+        System.out.println(l);
+
+        long l1 = l / (365*24 * 60 * 60 * 1000L);
+
+        if (l1 > 1L) {
+            System.out.println("时间是：" + l1);
+        }
 
     }
-
-
-
+    
     @Test
     public void test02() {
-     //测试&&  和  || 的联合使用
-        if (1==3 ||1==2   && 3==3){
+        //测试&&  和  || 的联合使用
+        if (1 == 3 || 1 == 2 && 3 == 3) {
             System.out.println("true");
-        }else {
+        } else {
             System.out.println("false");
         }
 
     }
 
 
+    @Test
+    public void test03() {
+        //测试&&  和  || 的联合使用
+        if (1 == 3 || 1 == 2 && 3 == 3) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
 
+    }
 }
 
 
