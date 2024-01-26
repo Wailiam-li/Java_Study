50f1431f5e6ad7f73d5931b1723ea72676339162
Wailiam-li <1374438718@qq.com>
2024-01-26 15:07:04 +0800

    2024.1.26

---
+++ src/JavaKnowledge/Date/localDate/LocalDateTest.java
@@ -0,0 +1,87 @@
+package JavaKnowledge.Date.localDate;
+
+import org.junit.Test;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * ClassName:LocalDate
+ * Description:        Java8日期类LocalDate、LocalTime、LocalDateTime的演示与方法使用
+ *
+ * @Date:2024/1/26 10:08
+ * @Author: 李威威
+ */
+public class LocalDateTest {
+
+    @Test
+    public void LocalDateTest() {
+
+        //1）创建方法
+        LocalDate now = LocalDate.now();                                      // 获取当前日期
+        LocalDate now2 = LocalDate.now(ZoneId.of("Asia/Shanghai"));      // 当前日期 (指定时区)
+        LocalDate now3 = LocalDate.now(Clock.systemDefaultZone());             // 当前日期 (指定时钟)
+        LocalDate localDate = LocalDate.of(2023, 1, 1); // 指定日期 2023-01-01
+
+        //2）获取方法
+        int year = now.getYear();                        // 获取年份
+        int month = now.getMonthValue();                // 获取月份（1-12）
+        Month monthEnum = now.getMonth();                // 获取月份的枚举值
+        int dayOfMonth = now.getDayOfMonth();            // 获取月份中的第几天（1-31）
+        int dayOfYear = now.getDayOfYear();                // 获取一年中的第几天（1-366）
+        System.out.println(dayOfYear);
+        DayOfWeek dayOfWeek = now.getDayOfWeek();        // 获取现在是星期几
+        int lengthOfYear = now.lengthOfYear();            // 获得当年总天数
+        int lengthOfMonth = now.lengthOfMonth();        // 获得当月总天数
+        long epochDay = now.toEpochDay();                // 与时间纪元（1970年1月1日）相差的天数
+        System.out.println("与时间纪元（1970年1月1日）相差的天数:"+epochDay+"天");
+
+        //3）运算方法
+        LocalDate localDate1 = now.plusDays(1);            // 给当前时间加一天
+        LocalDate localDate2 = now.plusDays(1);            // 给当前时间加一周
+        LocalDate localDate3 = now.plusMonths(1);        // 给当前时间加一月
+        LocalDate localDate4 = now.plusYears(1);        // 给当前时间加一年
+        LocalDate localDate5 = now.minusDays(1);        // 给当前时间减一天
+        LocalDate localDate6 = now.minusWeeks(1);        // 给当前时间减一周
+        LocalDate localDate7 = now.minusMonths(1);        // 给当前时间减一月
+        LocalDate localDate8 = now.minusYears(1);        // 给当前时间减一年
+
+
+        //4）修改方法
+        LocalDate localDate11 = now.withYear(2020);        // 修改日期对象年份为2020
+        LocalDate localDate22 = now.withMonth(1);        // 修改日期对象月份为1
+        LocalDate localDate33 = now.withDayOfMonth(1);    // 修改日期对象的日期(一月中的第几天)
+        LocalDate localDate44 = now.withDayOfYear(1);    // 修改日期对象的日期(一年中的第几天)
+    }
+
+       //5）比较方法
+        @Test
+        public void test () {
+            LocalDate now = LocalDate.now();
+            LocalDate localDate = LocalDate.of(2024, 8, 1);
+
+            boolean isBefore = localDate.isBefore(now);        // localDate是否在当天之前
+            System.out.println("isBefore:"+isBefore);
+            boolean isAfter = localDate.isAfter(now);        // localDate是否在当天之后
+            System.out.println("isAfter:"+isAfter);
+            boolean isEqual = localDate.isEqual(now);        // localDate是否在当天
+            System.out.println("isEqual:"+isEqual);
+            boolean isLeapYear = localDate.isLeapYear();    // localDate的年份是否为闰年,判断是否为闰年的方法：是否能被4或100整除，但不被400整除
+            System.out.println("isLeapYear:"+isLeapYear);
+
+
+        }
+
+
+    @Test   //LocalDate转String；
+    public void test02() {
+        LocalDate now = LocalDate.now();
+        String format = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        System.out.println(format);
+        System.out.println(now.getClass());
+        System.out.println(now);
+
+    }
+
+
+    }
---
+++ src/JavaKnowledge/Date/localDate/LocalTimeTest.java
@@ -0,0 +1,44 @@
+package JavaKnowledge.Date.localDate;
+
+import org.junit.Test;
+
+import java.time.*;
+
+/**
+ * ClassName:LocalTime
+ * Description:   演示LocalTime相关
+ *
+ * @Date:2024/1/26 13:59
+ * @Author: 李威威
+ */
+public class LocalTimeTest {
+
+    @Test
+    public void test(){
+        LocalTime now1 = LocalTime.now();							// 当前时间
+        LocalTime now2 = LocalTime.now(ZoneId.of("Asia/Shanghai"));	// 当前时间 (指定时区)
+        LocalTime now3 = LocalTime.now(Clock.systemDefaultZone());	// 当前时间 (指定时钟)
+        LocalTime localTime = LocalTime.of(9, 9, 9);				// 指定时间 09:09:09
+        System.out.println(now1);
+    }
+
+
+    @Test  //演示 LocalDateTime的创建
+    public void test02() {
+        // 1.当前日期
+        LocalDateTime now1 = LocalDateTime.now();
+        // 2.当前日期 (指定时区)
+        LocalDateTime now2 = LocalDateTime.now(ZoneId.of("Asia/Shanghai"));
+        // 3.当前日期 (指定时钟)
+        LocalDateTime now3 = LocalDateTime.now(Clock.systemDefaultZone());
+        // 4.指定日期 2023-01-01 01:01:00
+        LocalDateTime localDateTime1 = LocalDateTime.of(2023, 1, 1, 1, 1);
+        // 4.使用LocalDate和LocalTime对象创建LocalDateTime对象
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime2 = LocalDateTime.of(localDate, localTime);
+        System.out.println(now1);
+
+    }
+
+}
