cf97298567b18d0babc165ef0f7b8480ed3f19ec
Wailiam-li <1374438718@qq,com>
2024-01-31 19:59:23 +0800

    2024.1.3 周三下午学习

--- src/JavaKnowledge/a_draft/Test02.java
+++ src/JavaKnowledge/a_draft/Test02.java
@@ -2,6 +2,7 @@ package JavaKnowledge.a_draft;
 
 import org.junit.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -18,11 +19,25 @@ public class Test02 {
      * 演示获取时间的转换（DateTimeFormatter的相关使用）
      */
     @Test
-    public void test01(){
+    public void test01() {
         String yyyyMMddhhmmss = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").format(LocalDateTime.now());
         System.out.println(yyyyMMddhhmmss);
     }
 
+    /**
+     * 演示遇到闰年  2月29号的问题
+     */
+    @Test
+    public void test02() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String date = "2021-02-29";
+        try {
+            System.out.println(sdf.parse(date));
+        } catch (Exception e) {
+            System.out.println("日期格式不正确");
+        }
 
+
+    }
 
 }
