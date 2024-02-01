da31fccdc2e25db49ebb5827987d59a912c9e2ba
Wailiam-li <1374438718@qq.com>
2024-02-01 17:23:40 +0800

    2024.2.1

--- src/JavaKnowledge/a_draft/Per2.java
+++ src/JavaKnowledge/a_draft/Per2.java
@@ -6,14 +6,21 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+/**
+ * ClassName:Per2
+ * Description:
+ *
+ * @Date:2024/2/1 10:16
+ * @Author: 李威威
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Per {
+public class Per2 {
 
     private String name;
 
-    private Integer age;
+    private Date birthDate;
 
 
 }
