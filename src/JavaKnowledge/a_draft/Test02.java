4d9c159ca2b8334dabe0f587dae1adac0aa036f8
Wailiam-li <1374438718@qq,com>
2024-02-16 18:10:53 +0800

    2024.2.16 大年初七

--- src/JavaKnowledge/a_draft/Test02.java
+++ src/JavaKnowledge/a_draft/Test02.java
@@ -9,7 +9,6 @@ import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * ClassName:Test02
@@ -26,27 +25,27 @@ public class Test02 {
     @Test
     public void test01() {
         String yyyyMMddhhmmss = DateTimeFormatter.ofPattern("yyyyMMdd").format(LocalDateTime.now());
-        SimpleDateFormat sdf= new SimpleDateFormat("yyyyMMdd");
-        Date parse=null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        Date parse = null;
         try {
-             parse = sdf.parse(yyyyMMddhhmmss);
-        }catch (Exception e){
+            parse = sdf.parse(yyyyMMddhhmmss);
+        } catch (Exception e) {
         }
         System.out.println(parse);
     }
 
     @Test
     public void test02() {
-        int res=1;
-        res&=0;
+        int res = 1;
+        res &= 0;
         System.out.println(res);
 
     }
 
     @Test
     public void test03() {
-     //传一个date类型的日期，与当前时间进行比较；
-        Date date=new Date(1700409600L);
+        //传一个date类型的日期，与当前时间进行比较；
+        Date date = new Date(1700409600L);
         if (date.before(new Date())) {
             System.out.println("res");
         }
@@ -72,23 +71,25 @@ public class Test02 {
 
 
     @Test
-    public void test06() {
+    public void test07() {
         LocalDate localDate = LocalDate.now().minusMonths(1L);
         int year = localDate.getYear();
         Month month = localDate.getMonth();
         String mm = localDate.format(DateTimeFormatter.ofPattern("MM")); //这里居然还能根据 月的格式 单独 取月！！
-       
+
     }
 
     @Test   //测试contains()方法
-    public void test05() {
-       String a="aaaaa";
-       if (a.contains("b")){
-           System.out.println("666666");
-       }
+    public void test08() {
+        String a = "aaaaa";
+        if (a.contains("b")) {
+            System.out.println("666666");
+        }
+    }
+
 
     @Test   //随便玩玩
-    public void test06() {
+    public void test09() {
         System.out.print("请输入你喜欢的人的名字：");
         Scanner sc = new Scanner(System.in);
         String next = sc.nextLine();
@@ -98,7 +99,7 @@ public class Test02 {
 
 
     @Test
-    public void test07() {
+    public void test10() {
         Per2 pp = new Per2("lili22", new Date(1706494220000L));  //大前天：1.29
         Per2 p1 = new Per2("lili", new Date(1706580620000L));  //前天：1.30
         Per2 p2 = new Per2("liwei", new Date(1706667020000L));  //昨天：1.31
@@ -119,7 +120,7 @@ public class Test02 {
     }
 
     @Test
-    public void test08() {
+    public void test11() {
         Date date1 = new Date(1706494220000L);  //注意new Date()需要传时间的 毫秒值！
         System.out.println(date1);
         Date date2 = new Date(1706580620000L);
