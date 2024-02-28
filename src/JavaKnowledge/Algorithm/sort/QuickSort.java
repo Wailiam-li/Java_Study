36bae2f8fa870958a98e96ec0288698da9284005
Wailiam-li <1374438718@qq.com>
2024-02-28 17:39:45 +0800

    2024.2.27 周二

--- src/JavaKnowledge/Algorithm/sort/QuickSort.java
+++ src/JavaKnowledge/Algorithm/sort/QuickSort.java
@@ -4,11 +4,26 @@ package JavaKnowledge.Algorithm.sort;
  * ClassName:QuickSort
  * Description:   快排
  *
+ *    核心思想：取第一个数为基准值，将整个数组以基准值分成两堆，小于基准值的和大于基准值的，并将此基准值插入到两堆中间；
+ *    具体： 从第二个索引开始为start，最后一个索引为end，都与基准值进行比较，以从小到大为例，直到满足：左边的比基准值大，
+ *          右边的比基准值小，则进行start与end的对应的值交换，直到start与end相遇，则本轮结束，将基准值送入到start（end）的位置
+ *
  * @Date:2024/2/27 11:47
  * @Author: 李威威
  */
 public class QuickSort {
 
+    public static void main(String[] args) {
+        int[] arr={3,1,2,5,4};
 
+
+
+    }
+
+    public static void quickSort(int[] arr,int i,int j) {
+        //首先
+
+
+    }
 
 }
