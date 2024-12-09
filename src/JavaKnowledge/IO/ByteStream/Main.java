019952b5aa86d46b7d227bd262123c59c9e76159
Liweiwei <1374438718@qq.com>
2024-12-09 23:09:15 +0800

    2024.11.5  周一 晚上 学习

--- src/JavaKnowledge/IO/ByteStream/Main.java
+++ src/JavaKnowledge/IO/ByteStream/Main.java
@@ -1,6 +1,5 @@
 package JavaKnowledge.IO.ByteStream;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -17,9 +16,9 @@ public class Main {
         释放资源;
         */
         //创建对象；       相当于建立起来了一条通道路径            //这里用了绝对路径
-        FileOutputStream fos = new FileOutputStream("C:\\Users\\李威威\\IdeaProjects\\Java_Study\\src\\JavaKnowledge\\IO\\a.txt");
+        FileOutputStream fos = new FileOutputStream("C:\\Users\\Liweiwei\\IdeaProjects\\Java_Study\\src\\JavaKnowledge\\IO\\ByteStream\\a.txt");
         //写出数据；       相当于程序往这条路径上送东西
-        fos.write(97);  //这里会转化成对应的的ASSCI码
+        fos.write(98);  //这里会转化成对应的的ASSCI码
         //释放资源;       相当于将这条路径关闭      注：如果不关闭，相当于一直在打开这个文件，占用（内存）资源，这样就删除不了
         fos.close();
 
